b99a2874f95bfbacee91543b6bf414c0b1f47b99
ccy <303752792@qq.com>
2024-04-03 17:40:06 +0800

    dev

--- src/dw/control/common.tsx
+++ src/dw/control/common.tsx
@@ -1095,31 +1095,31 @@ export const DIGITAL_FORMAT_PROP: ControlProProps = {
 
 // 新增-图表基本信息
 export const CHART_BASE_INFO: any = {
-  id: 'echart',
+  id: 'echartOption',
   name: '基本信息',
   defaultOpen: true,
   nodes: [
     {
       visible: true,
-      id: 'echart.version',
+      id: 'echartOption.version',
       name: '版本',
       editor: { component: 'Input' },
     },
     {
       visible: true,
-      id: 'echart.versionDesc',
+      id: 'echartOption.versionDesc',
       name: '版本描述',
       editor: { component: 'Input' },
     },
     {
       visible: true,
-      id: 'echart.pluginName',
+      id: 'echartOption.pluginName',
       name: '插件标识',
       editor: { component: 'Input' },
     },
     {
       visible: true,
-      id: 'echart.isPublish',
+      id: 'echartOption.isPublish',
       name: '是否发布',
       editor: {
         component: 'Select',
