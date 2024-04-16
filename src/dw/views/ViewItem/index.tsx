529f085f022348db2694e0203db5e308f19885c9
ccy <303752792@qq.com>
2024-04-16 19:55:19 +0800

    dev

--- src/dw/views/ViewItem/index.tsx
+++ src/dw/views/ViewItem/index.tsx
@@ -1,60 +1,60 @@
-import React, { FC, useEffect, useContext } from 'react'
-import { RecoilRoot } from 'recoil'
-import 'dw/style/reset.less'
-import '@kdcloudjs/kdesign/dist/kdesign.css'
-import 'dw/style/reset-kdesign.less'
-import JSON from '../../../../mock/PropsDataType/DATA_INIT.json'
+import React, { FC, useEffect, useContext } from "react";
+import { RecoilRoot } from "recoil";
+import "dw/style/reset.less";
+import "@kdcloudjs/kdesign/dist/kdesign.css";
+import "dw/style/reset-kdesign.less";
+import JSON from "../../../../mock/PropsDataType/DATA_INIT.json";
 // main
-import './index.less'
-import Design from 'dw/views/Design/Design'
-import useMain from '@/dw/store/useMain'
+import "./index.less";
+import Design from "dw/views/Design/Design";
+import useMain from "@/dw/store/useMain";
 
 export interface ViewItemProps {
-  model: any
-  propsData: any
-  getLangMsg: any
-  invoke: any
-  invokeAsync: any
-  designMode: boolean
+    model: any;
+    propsData: any;
+    getLangMsg: any;
+    invoke: any;
+    invokeAsync: any;
+    designMode: boolean;
 }
 
 export const defaultViewItemContext: ViewItemProps = {
-  model: { test: true },
-  propsData: {},
-  getLangMsg: () => {},
-  invoke: () => {},
-  invokeAsync: () => {},
-  designMode: true,
-}
+    model: { test: true },
+    propsData: {},
+    getLangMsg: () => {},
+    invoke: () => {},
+    invokeAsync: () => {},
+    designMode: true
+};
 
-export const ViewItemContext = React.createContext<ViewItemProps>(defaultViewItemContext)
+export const ViewItemContext = React.createContext<ViewItemProps>(defaultViewItemContext);
 
 const BaseView = () => {
-  const { initPage } = useMain()
+    const { initPage } = useMain();
 
-  useEffect(() => {
-    // initPage(JSON)
-  }, [])
+    useEffect(() => {
+        initPage(JSON);
+    }, []);
 
-  return (
-    <div className="dw-view-item">
-      <Design />
-    </div>
-  )
-}
+    return (
+        <div className='dw-view-item'>
+            <Design />
+        </div>
+    );
+};
 
-const ViewItem: FC<any> = (props) => {
-  const value = {
-    ...props,
-  }
+const ViewItem: FC<any> = props => {
+    const value = {
+        ...props
+    };
 
-  return (
-    <RecoilRoot>
-      <ViewItemContext.Provider value={value}>
-        <BaseView />
-      </ViewItemContext.Provider>
-    </RecoilRoot>
-  )
-}
+    return (
+        <RecoilRoot>
+            <ViewItemContext.Provider value={value}>
+                <BaseView />
+            </ViewItemContext.Provider>
+        </RecoilRoot>
+    );
+};
 
-export default ViewItem
+export default ViewItem;
