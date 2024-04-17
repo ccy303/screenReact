db60178f9d8e889450fa6c65828b31222350b435
ccy <303752792@qq.com>
2024-04-17 16:55:44 +0800

    dev

--- src/dw/components/item/Table/index.tsx
+++ src/dw/components/item/Table/index.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useMemo, useRef } from 'react'
-import KdCard from 'dw/components/common/KdCard'
-import { Table } from '@kdcloudjs/kdesign'
-import _ from 'lodash'
+import React, { useEffect, useMemo, useRef } from "react";
+import KdCard from "dw/components/common/KdCard";
+import { Table } from "@kdcloudjs/kdesign";
+import _ from "lodash";
 
 const Chart = (item: any) => {
-  const showTitle = useMemo(() => item && item.content && item.content.title && item.content.title.show, [item])
+    const showTitle = useMemo(() => item && item.content && item.content.title && item.content.title.show, [item]);
 
-  const ref: any = useRef(null)
+    const ref: any = useRef(null);
 
-  return (
-    <KdCard item={item} showTitle={showTitle}>
-      <Table style={{ width: '100%', height: '100%' }} dataSource={[]} columns={[]} ref={ref} />
-    </KdCard>
-  )
-}
+    return (
+        <KdCard item={item} showTitle={showTitle}>
+            <Table style={{ width: "100%", height: "100%" }} dataSource={item.dataSource || []} columns={item.columns || []} ref={ref} />
+        </KdCard>
+    );
+};
 
-export default Chart
+export default Chart;
