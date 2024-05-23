1394621ce94353be942204f45bb56d06b2a84fb6
CatTomAjax <karlgm123@163.com>
2024-05-23 14:07:35 +0800

    55版本号

--- src/dw/main.js
+++ src/dw/main.js
@@ -64,7 +64,7 @@ import JSON from "../../mock/PropsDataType/DATA_INIT.json";
             this.root = createRoot(model.dom);
         },
         init: function (props) {
-            console.log("version", 54);
+            console.log("version", 55);
             console.log("-----init", this.model, props);
             this.model.dom.style.height = "100vh";
             this.model.dom.style.width = "100vw";
