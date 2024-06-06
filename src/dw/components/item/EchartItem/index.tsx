29794ec4f8b58beb8be1cae63081ba521f514150
CatTomAjax <karlgm123@163.com>
2024-06-06 20:25:25 +0800

    git

--- src/dw/components/item/EchartItem/index.tsx
+++ src/dw/components/item/EchartItem/index.tsx
@@ -809,20 +809,27 @@ export default React.memo(
             }
         }, []);
 
+        useEffect(() => {
+            const parent = ref.current.ele.parentElement;
+            const padding = (getComputedStyle(parent).padding || "0").replace("px", "");
+            setEchartStyle({
+                width: `${parent.clientWidth - Number(padding) * 2}px`,
+                height: `${parent.clientHeight - Number(padding) * 2}px`
+            });
+        }, []);
+
         return (
             <KdCard item={item} showTitle={showTitle}>
                 {showLoading ? (
                     <Spin type='page' spinning={showLoading} style={{ width: "100%", height: "100%", justifyContent: "center" }}></Spin>
                 ) : (
-                    <div style={{ width: "100%", height: "100%" }}>
-                        <ReactECharts
-                            key={echartKey}
-                            style={{ width: "100%", height: "100%" }}
-                            option={{ ...chartOption }}
-                            ref={ref}
-                            onEvents={{ click: onChartClick }}
-                        />
-                    </div>
+                    <ReactECharts
+                        key={echartKey}
+                        style={customProps.isShow ? echartStyle : { width: "100%", height: "100%" }}
+                        option={{ ...chartOption, animationEasing: "cubicInOut", animationDuration: 2000 }}
+                        ref={ref}
+                        onEvents={{ click: onChartClick }}
+                    />
                 )}
             </KdCard>
         );
